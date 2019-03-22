cd8a662df0c6a530b0bcc1fe4d3a36745233fd93
Dylan Maghe <maghe.dylan@gmail.com>
2019-03-22 11:12:03 +0100

    ajouts des différent fichiers

---
+++ assets/js/footer.js
@@ -0,0 +1,12 @@
+let randomTest= parseInt(Math.random() * 100);
+
+let test2 = () => {
+    if(randomTest < 99) {
+        console.log("Hello ici c'est le footer!");
+    }
+    else {
+        console.log('Hello les Turing!!');
+    }
+}
+
+test2();
---
+++ assets/js/header.js
@@ -0,0 +1,13 @@
+let random= parseInt(Math.random() * 100);
+
+
+let test1 = () => {
+    if(random < 99) {
+        console.log("Hello ici c'est le header!!");
+    }
+    else {
+        console.log("Hello les Turing!!");
+    }
+}
+
+test1();
